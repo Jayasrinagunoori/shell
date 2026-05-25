88c19c0481de6f12c882e6ae137eefadeae699f7
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-25 16:54:52 +0530

    fifth commit

---
+++ 07-data-types.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUM1=10
+NUM2=20
+
+SUM=$(($NUM1+$NUM2))
+
+echo "Sum of two numbers: $SUM"
+
+# Array
+MOVIES=("RRR" "VARANASI" "PUSHPA")
+
+echo "Movies are : ${MOVIES[@]}"
+echo "First Movie : ${MOVIES[0]}"
+echo "Second Move : ${MOVIES[1]}"
