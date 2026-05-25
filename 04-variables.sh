8bde1e34ca6f1f908936ebe0d59242202c6f3c74
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-25 16:12:05 +0530

    fifth commit

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Please enter your username::"
+read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variabl
+
+echo "Username is $USER_NAME"
+
