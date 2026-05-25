dd653fb730c124e372c32a639288468093b2fdb0
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-25 16:57:16 +0530

    fifth commit

---
+++ 08-conditions.sh
@@ -0,0 +1,17 @@
+
+NUMBER=$1
+
+# gt - greater than
+# lt - less than
+# eq - equal
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
+
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number $NUMBER is greater than 20"
+elif [ $NUMBER -ge 20 ]; then
+    echo "Given number $NUMBER is equal to 20"  
+else
+    echo "Give number $NUMBER is less than 20"      
+fi 
