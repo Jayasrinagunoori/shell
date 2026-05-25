daaec721d1659c51f9097e75b9985bc546c9c5be
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-25 16:43:21 +0530

    fifth commit

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -10,3 +10,10 @@ echo "Who is running this : $USER"
 echo "Which directory: $PWD"
 echo "Home directory: $HOME"
 echo "PID of current script: $$"
+sleep 5 &
+echo "PID of background command running just now : $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random Number: $RANDOM"
+echo "Exit code of previous command: $?"
