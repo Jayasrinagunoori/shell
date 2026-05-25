32b6cb62ea73a8355c3e73e927cfe128764eeea6
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-25 16:19:42 +0530

    sixth commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,7 +7,7 @@ read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stor
 echo "Username is $USER_NAME"
 
 echo "Please enter your password::"
-read  PASSWORD
+read -s PASSWORD
 
 echo "Password is : $PASSWORD"
 
