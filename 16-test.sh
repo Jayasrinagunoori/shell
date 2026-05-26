cc130646c7e427e39bf09e7cd9a9db4143d1e570
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-26 19:51:35 +0530

    third commit

--- 16-test.sh
+++ 16-test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-NUM1=$1
-NUM2=$2
+# Read numbers from user
+echo "Enter first number:"
+read NUM1
 
-SUM=$((echo $($(NUM1)+$($(NUM2))))
+echo "Enter second number:"
+read NUM2
+
+# Calculate sum
+SUM=$((NUM1 + NUM2))
 
 echo "Sum of two numbers: $SUM"
 
 # Array
 MOVIES=("RRR" "VARANASI" "PUSHPA")
 
-echo "Movies are : ${MOVIES[@]}"
-echo "First Movie : ${MOVIES[0]}"
-echo "Second Move : ${MOVIES[1]}"
+echo "Movies are: ${MOVIES[@]}"
+echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
