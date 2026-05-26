6afe97d78d51075e9d6a101b4d588918834bdacc
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-26 19:58:52 +0530

    third commit

--- 16-test.sh
+++ 16-test.sh
@@ -12,22 +12,21 @@ SUM=$((NUM1 + NUM2))
 
 echo "Sum of two numbers: $SUM"
 
-# Array
-
+# Read movie names
 echo "Enter first Movie name:"
-
 read Movie0
 
-echo "Enter Second name :"
-
+echo "Enter second Movie name:"
 read Movie1
 
-echo "Enter Third Movie number:"
-
+echo "Enter third Movie name:"
 read Movie2
 
-MOVIES=("Movie0" "Movie1" "Movie2")
+# Store values in array
+MOVIES=("$Movie0" "$Movie1" "$Movie2")
 
+# Display array values
 echo "Movies are: ${MOVIES[@]}"
 echo "First Movie: ${MOVIES[0]}"
 echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
