85952281ce940b68822daf293a0eec8e7d80fbeb
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-26 19:37:21 +0530

    third commit

---
+++ 16-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUM1=$1
+NUM2=$2
+
+SUM=$(($NUM1+$NUM2))
+
+echo "Sum of two numbers: $SUM"
+
+# Array
+MOVIES=("RRR" "VARANASI" "PUSHPA")
+
+echo "Movies are : ${MOVIES[@]}"
+echo "First Movie : ${MOVIES[0]}"
+echo "Second Move : ${MOVIES[1]}"
