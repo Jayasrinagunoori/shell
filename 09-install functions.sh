502bded7667a01cc9315426ece8fdfe4b4a6cde6
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-03 16:15:29 +0530

    fifth commit

--- 09-install functions.sh
+++ 09-install functions.sh
@@ -4,11 +4,11 @@ USERID=$(id -u)
 
 # Check root access or not
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
-    exit 1
+    echo "Plase run the script with root access"
 
-fi
-VALIDATE(){
+    fi
+
+    VALIDATE (){
     if [ $2 -ne 0 ]; then
         echo "Installing $1 is ... FAILED"
         exit 1
@@ -16,3 +16,36 @@ VALIDATE(){
         echo "Installing $1 is ... SUCCESS"
     fi
 }
+# echo "I am continuing..."
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING"
+else
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?
+fi
+
+    echo "I Am Contining..."
+    dnf list instlled Mysql
+
+if [$? -eq 0]; then
+    echo  "MySql is already installed... SKIPPING"
+else 
+    echo "Valiadte Mysql $"?
+
+    fi
+
+dnf list insatll nginx
+
+if[$? -eq 0];then
+
+echo "Nginx is already installed...SKIPPING"
+
+else
+echo "Installing nginx"
+dnf install nginx -y
+VALIDATE nginx $?
+
+fi
