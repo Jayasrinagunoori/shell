3ac68fec908bffae6ad9364e708ba9e133e5ad0d
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-03 16:03:53 +0530

    fifth commit

--- 09-install functions.sh
+++ 09-install functions.sh
@@ -6,3 +6,13 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
+
+fi
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
