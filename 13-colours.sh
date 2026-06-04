4187af92f1db389bac64ecc55fc1d16de0fae6f1
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 20:08:25 +0530

    six commit

--- 13-colours.sh
+++ 13-colours.sh
@@ -7,13 +7,23 @@ if [ $USERID -ne 0 ]; then
     exit1
 fi
 
+VALIDATE (){
+    if [ $2 -ne 0 ]; then
+        echo  "installing $1 is FAILED
+        exit1
+    
+        else 
+        echo "Installing $1 is SUCCESS"
+    fi
+}
+
 for package in $@
 do 
   echo "Installing $package"
   dnf list installed $package
   if [ $? -ne 0 ]; then
     dnf install $package -y
-    echo "$package installed"
+    VALIDATE "$package"
 
   else
     echo " $package already installed...SKIPPING"
