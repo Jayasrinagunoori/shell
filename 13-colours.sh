7327811bdc304336c3dba5824b1ac7ab88343525
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 20:34:18 +0530

    six commit

--- 13-colours.sh
+++ 13-colours.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 R="\e[31m"
 G="\e[32m"
@@ -16,17 +17,17 @@ fi
 
 VALIDATE (){
     if [ $2 -ne 0 ]; then
-        echo -e "Installing $1 is $R FAILED $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [Error] Installing $1 is $R FAILED $N" | tee -a $LOGS_FILE
         exit1
     
         else 
-        echo -e "Installing $1 is $Y SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP[INFO] Installing $1 is $Y SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
 for package in $@
 do 
-  echo "$G Installing $N $package"
+  echo "$TIMESTAMP [INFO] $G Installing $N $package"
   dnf list installed $package &>> $LOGS_FILE
   if [ $? -ne 0 ]; then
     dnf install $package -y &>> $LOGS_FILE
