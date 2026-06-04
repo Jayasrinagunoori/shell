93862419493d22d7b1d39dabbc1cbdc249060a4d
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 20:17:49 +0530

    six commit

--- 13-colours.sh
+++ 13-colours.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ $USERID -ne 0 ]; then
     echo "please run this script using root access"
@@ -9,20 +11,20 @@ fi
 
 VALIDATE (){
     if [ $2 -ne 0 ]; then
-        echo  "Installing $1 is FAILED"
+        echo -e "Installing $1 is FAILED" | tee -a $LOGS_FILE
         exit1
     
         else 
-        echo "Installing $1 is SUCCESS"
+        echo -e "Installing $1 is SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
 for package in $@
 do 
   echo "Installing $package"
-  dnf list installed $package
+  dnf list installed $package &>> $LOGS_FILE
   if [ $? -ne 0 ]; then
-    dnf install $package -y
+    dnf install $package -y &>> $LOGS_FILE
     VALIDATE "$package" $?
 
   else
