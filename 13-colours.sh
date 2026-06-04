df395d2bb580b986e6e3ca7c1f4ca83714f4e3e2
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 19:50:31 +0530

    six commit

--- 13-colours.sh
+++ 13-colours.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
 echo "please run this script using root access"
 exit1
 fi
@@ -11,7 +11,7 @@ for package in $@
 do 
   echo "Installing $package"
   dnf list installed $package
-  if [$? -ne 0]; then
+  if [ $? -ne 0 ]; then
   dnf install package -y
   VALIDATE "$package" $?
 
