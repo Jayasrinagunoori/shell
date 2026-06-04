174efc4a19fd01b34388406c966e4090cab59a5d
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 19:59:07 +0530

    six commit

--- 13-colours.sh
+++ 13-colours.sh
@@ -3,8 +3,8 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-echo "please run this script using root access"
-exit1
+    echo "please run this script using root access"
+    exit1
 fi
 
 for package in $@
@@ -12,10 +12,10 @@ do
   echo "Installing $package"
   dnf list installed $package
   if [ $? -ne 0 ]; then
-  dnf install package -y
-  echo "$package installed"
+    dnf install $package -y
+    echo "$package installed"
 
-else
-echo " $package already installed...SKIPPING"
+  else
+    echo " $package already installed...SKIPPING"
 fi
 done
