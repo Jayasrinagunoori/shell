23ba6e3c03ccca5ac32b0cf7f7b85af8d01cc3c8
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-04 19:48:10 +0530

    six commit

---
+++ 13-colours.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [USERID -ne 0]; then
+echo "please run this script using root access"
+exit1
+fi
+
+for package in $@
+do 
+  echo "Installing $package"
+  dnf list installed $package
+  if [$? -ne 0]; then
+  dnf install package -y
+  VALIDATE "$package" $?
+
+else
+echo " $package already installed...SKIPPING"
+fi
+done
